58e3fd5396fcf1f6b7bb8c4b5c282e716dbc6eae
Ravindra Devrani <ravindra.devrani003@gmail.com>
2022-09-22 14:21:24 +0530

    data binding added

---
+++ src/app/component/first/first.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+
+  name:string="john doe";
+
+  isHidden =false;
+
+  toggle(){
+     this.isHidden=!this.isHidden;
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
